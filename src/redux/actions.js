d2d71a907802fec882568ca19315deddf24a8b23
SNasser97 <snasserahmed@outlook.com>
2020-04-11 15:59:24 +0100

    async await syntactic sugar for request actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -15,52 +15,51 @@ export const setSearchField = (text) => {
   }
 }
 
-export const requestRandomEpisode = () => (dispatch) => {
+export const requestRandomEpisode = () => async (dispatch) => {
   const url = "https://listen-api.listennotes.com/api/v2/just_listen"; 
-  dispatch({type: REQUEST_RAND_EPISODE_PENDING});
-  fetch(url, {
-    headers:{
-      "Content-Type":"application/json",
-      "X-ListenAPI-Key":`${process.env.REACT_APP_API_KEY}`
-    }
-  })
-  .then(resp => resp.json())
-  .then(data => {
-    // For showing the skel loader
-    setTimeout(()=>dispatch({
+  
+  try {
+    dispatch({type: REQUEST_RAND_EPISODE_PENDING});
+    const resp = await fetch(url, {
+      headers:{
+        "Content-Type":"application/json",
+        "X-ListenAPI-Key":`${process.env.REACT_APP_API_KEY}`
+      }
+    })
+    const respData = await resp.json();
+    // display skel-loader 3 seconds
+    setTimeout(()=> dispatch({
       type: REQUEST_RAND_EPISODE_SUCCESS,
-      payload: data
+      payload: respData
     }), 3000);
-  })
-  .catch(error => {
+  } catch(error) {
     dispatch({
       type:REQUEST_RAND_EPISODE_FAILED,
       payload:error
     })
-  })
+  }
 }
 
-export const requestEpisodes = (urlSearch, urlOffset) => (dispatch) => {
-  
+export const requestEpisodes = (urlSearch, urlOffset) => async (dispatch) => {
   const url =  `https://listen-api.listennotes.com/api/v2/search?q=${urlSearch}&offset=${urlOffset ? urlOffset : 0}&scope=episode&language=Any language&len_min=0`
-  console.log("current url=>", url);
-  dispatch({type: REQUEST_EPISODE_PENDING});
-  fetch(url, {
-     headers:{
-      "Content-Type":"application/json",
-      "X-ListenAPI-Key":`${process.env.REACT_APP_API_KEY}`
-    }
-  })
-  .then(resp=>resp.json())
-  .then(data => {
-      dispatch({
+  
+  try {
+    dispatch({type: REQUEST_EPISODE_PENDING});
+    const resp = await fetch(url, {
+       headers:{
+        "Content-Type":"application/json",
+        "X-ListenAPI-Key":`${process.env.REACT_APP_API_KEY}`
+      }
+    })
+    const respData = await resp.json();
+    dispatch({
         type:REQUEST_EPISODE_SUCCESS,
-        payload:data
+        payload:respData
       })
-  }).catch(error => {
-      dispatch({
+  } catch(error) {
+     dispatch({
         type:REQUEST_EPISODE_FAILED,
         payload: error
       })
-  })
+  }
 }
