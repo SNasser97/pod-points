def7c2dbea4c75f2d9ac44e704a8723c36fb5f82
SNasser97 <snasserahmed@outlook.com>
2020-05-14 18:47:51 +0100

    add disclaimer in footer + remove specified width from nav buttons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,17 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__author fs--4">
         <p className="footer__authorName">
-          Built by
+          Web app built by
           <a href="https://github.com/SNasser97" className="footer__github">
             Nasser <i className="fab fa-github"></i>
           </a>
         </p>
+      </div>
+      <div className="footer__disclaimer">
+        <div className="footer__disclaimerIcon">
+          <i className="fas fa-info-circle"></i>
+        </div>
+        <sub className="footer__disclaimerText">This is just a personal project demonstrating my current abilities and isn't intended for commercial gain</sub>
       </div>
       <div className="footer__legal">
         <figure className="footer__legalFigure">
