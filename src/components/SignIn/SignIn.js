156ef642e9757986dbc04bcf84db194f3d283e27
SNasser97 <snasserahmed@outlook.com>
2020-06-18 19:49:22 +0100

    connect signin component to redux store  and create dispatch for user sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setUsernameText, setPasswordText, signIn } from "../../redux/actions"
 
+const mapStateToProps = (state) => {
+   const { getUsernameText, getPasswordText } = state // reducers
+  return {
+    usernameField: getUsernameText.usernameField,
+    passwordField: getPasswordText.passwordField,
+  }
+}
+const mapDispatchToProps = (dispatch) => { // dispatch the action
+  return {
+    onUserFieldChange: (e) => dispatch(setUsernameText(e.target.value)),
+    onPassFieldChange: (e) => dispatch(setPasswordText(e.target.value)),
+    handleUserSignIn: (user,pass) => dispatch(signIn(user, pass))
+  }
+}
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
   }
 
-  
   render() {
+    const { 
+      onPassFieldChange, 
+      onUserFieldChange,
+      handleUserSignIn, 
+      usernameField, 
+      passwordField
+    } = this.props
+    // todo: handleSignIn action
+    console.log(handleUserSignIn)
     return (
       <main>
         <div className="container">
@@ -16,41 +40,38 @@ class SignIn extends Component {
             <fieldset className="signin__header">
               <p className="signin__title fs--1">Sign in</p>
               <div className="signin__box fs--5">
-                <label
-                  className="signin__label signin__label--username has_color--darkgrey4"
-                  htmlFor="username"
-                >
+                <label className="signin__label signin__label--username has_color--darkgrey4" htmlFor="username">
                   Username
-              </label>
+                </label>
                 <input
                   className="signin__input  fs--4"
                   name="username"
                   type="text"
                   autoComplete="off"
                   required
+                  onChange={onUserFieldChange} // listen for change on input username
                 />
                 <hr className="signin__line" />
               </div>
               <div className="signin__box fs--5">
-                <label
-                  className="signin__label signin__label--password has_color--darkgrey4"
-                  htmlFor="password"
-                >
+                <label className="signin__label signin__label--password has_color--darkgrey4" htmlFor="password">
                   Password
-              </label>
+                </label>
                 <input
                   className="signin__input fs--4"
                   name="password"
                   type="password"
                   autoComplete="off"
                   required
+                  onChange={onPassFieldChange} // listen for change on input username
                 />
                 <hr className="signin__line" />
               </div>
             </fieldset>
             <div className="signin__btn ">
               <Link to="/home">
-                <input className="btn btn__full" type="submit" value="Sign In" />
+                <input onClick={() => handleUserSignIn(usernameField,passwordField)} 
+                className="btn btn__full" type="submit" value="Sign In" />
               </Link>
             </div>
             <div className="form__link form__link--register fs--3">
@@ -69,4 +90,5 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+// export default SignIn;
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
