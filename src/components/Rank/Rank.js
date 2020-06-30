38de730a61a32fc6dbf5cf660e243bf3c9c28366
SNasser97 <snasserahmed@outlook.com>
2020-06-30 16:01:51 +0100

    display appropriate score based on set condition

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Rank = ({user}) => {
+const Rank = ({user, score}) => {
+	// displays current score and if score is updated will show new score once user finishes episode.
 	return (
 		<div className="user">
 			<div className="user__details">
@@ -8,7 +9,7 @@ const Rank = ({user}) => {
 				  Hi, <span className="user__rank--name">{user.username}</span>
 				</h2>
 				<p className="user__info fs--3">
-				  You have a total score of <span className="user__rank--score">{user.score}</span> points!
+				  You have a total score of <span className="user__rank--score">{!score ? user.score : score}</span> points!
 				</p>
 				<div className="user__info">
 					<a href="#!" className="btn btn__ghost user__btn">View leaderboard</a>
